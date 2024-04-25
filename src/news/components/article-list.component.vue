3ddbed6fbd04dfc34db65a407d35671b210854c3
VelardeSoft <u20211c221@upc.edu.pe>
2024-04-25 18:34:58 -0500

    feat(news-articles): added component for article list.

---
+++ src/news/components/article-list.component.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "article-list",
+  props:{
+    articles: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div v-for="article in articles">
+    <article-card article="article"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
