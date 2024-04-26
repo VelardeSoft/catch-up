cfc7740ab6a9a712edbe1f0ade928d2ff685cb4c
VelardeSoft <u20211c221@upc.edu.pe>
2024-04-25 19:33:00 -0500

    feat(news-sources): added component for side menu of sources-avance.

---
+++ src/app.vue
@@ -0,0 +1,37 @@
+<script>
+import {NewsApiService} from "./news/services/news-api.service.js";
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      sidebarVisible: false,
+      articles: [],
+      errors: [],
+      newsApi: new NewsApiService()
+    }
+  },
+  created(){
+    //TODO: Fetch news articles for default source
+  },
+  methods: {
+    // Build article list from response data
+    buildArticleListFromResponseData(articles){
+      return articles.map(article =>
+          new Article(article.title,
+              article.description,
+              article.url,
+              article.urlToImage,
+              article.source));
+    },
+    // Fetch articles for selected source
+    getArticlesForSource(sourceId){
+      this.newsApi.getArticlesForSource(sourceId)
+          .then(response => {
+            let articles = response.data.articles;
+            // 1:24:07
+          })
+    }
+  }
+}
+</script>
